29dac2bfd5e78e93a5dee12d5e1b94cd2cf6686c
YuliaKorchahina <ruda_nokia@ukr.net>
2022-12-08 13:47:40 +0200

    task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const list = galleryItems
+  .map(
+    (galleryItems) =>
+      `<a class="gallery__link" href='${galleryItems.original}'>
+    <img
+    class="gallery__image" 
+    src='${galleryItems.preview}'
+    alt='${galleryItems.description}' width="340"
+    data-source='${galleryItems.original}'
+    />
+    </a>`
+  )
+  .join(" ");
+
+galleryEl.insertAdjacentHTML("afterbegin", list);
+//------------------------------------style------------------------------------
+galleryEl.style.display = "flex";
+galleryEl.style.listStyle = "none";
+galleryEl.style.justifyContent = "center";
+galleryEl.style.flexWrap = "wrap";
+// ------------------------------------2------------------------------------
+galleryEl.addEventListener("click", onClick);
+function onClick (event) {
+  event.preventDefault();
+  console.log(event.target);
+  console.log(event.currentTarget);
+ 
+};
